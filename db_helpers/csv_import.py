55c3af998fa4b53887067908c592434e8f130a1e
jiri.one <nemec@jiri.one>
2020-07-20 21:41:27 +0200

    finished script for import categories from csv to rethinkdb

--- db_helpers/csv_import.py
+++ db_helpers/csv_import.py
@@ -8,14 +8,14 @@ conn = r.connect( "192.168.222.20", 28015).repl()
 topics = r.db("blog_jirione").table("topics")
 posts = r.db("blog_jirione").table("posts")
 
-with open('zapisky.csv', encoding="utf-8") as csvfile:
-    posts.delete().run(conn)
-    reader = 0
-    fieldnames = ['cislo', 'url', 'nadpis', 'obsah', 'zadano_kdy', 'pocet_komentaru', 'kategorie']
-    reader = csv.DictReader(csvfile, fieldnames=fieldnames, delimiter=';')
-    sorted_reader = sorted(reader, key=lambda row: row["zadano_kdy"], reverse=False)
-    for row in sorted_reader[:-1]: 
-        posts.insert({'comments': row['pocet_komentaru'], 'when': row['zadano_kdy'], 'url': row['url'], 'header': row['nadpis'], 'content': row['obsah'], 'categories': row['kategorie']})    
+#with open('zapisky.csv', encoding="utf-8") as csvfile:
+    #posts.delete().run(conn)
+    #reader = 0
+    #fieldnames = ['cislo', 'url', 'nadpis', 'obsah', 'zadano_kdy', 'pocet_komentaru', 'kategorie']
+    #reader = csv.DictReader(csvfile, fieldnames=fieldnames, delimiter=';')
+    #sorted_reader = sorted(reader, key=lambda row: row["zadano_kdy"], reverse=False)
+    #for row in sorted_reader[:-1]: 
+        #posts.insert({'comments': row['pocet_komentaru'], 'when': row['zadano_kdy'], 'url': row['url'], 'header': row['nadpis'], 'content': row['obsah'], 'categories': row['kategorie']})    
 
 
 with open('kategorie.csv', encoding="utf-8") as csvfile:
@@ -24,12 +24,11 @@ with open('kategorie.csv', encoding="utf-8") as csvfile:
     fieldnames = ['kategorie', 'popis', 'cislo', 'url_kategorie']
     reader = csv.DictReader(csvfile, fieldnames=fieldnames, delimiter=';')
     for row in list(reader)[1:]:
-        info = topics.insert(
-            [
-                {'topic': {"cze": row['kategorie'], "eng": ""}},
-                {'url': {"cze": row['url_kategorie'], "eng": ""}},
-                {'description': {"cze": row['popis'], "eng": ""}}
-            ]).run(conn)
+        info = topics.insert({
+            'topic': {"cze": row['kategorie'], "eng": ""},
+            'url': {"cze": row['url_kategorie'], "eng": ""},
+            'description': {"cze": row['popis'], "eng": ""}
+            }).run(conn)
 
 #fieldnames = ['cislo', 'url', 'nadpis', 'obsah', 'zadano_kdy', 'pocet_komentaru', 'kategorie']        
 #posts.delete().run(conn)
