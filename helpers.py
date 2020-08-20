f84efdd5f14c8e559d64ccee4b7f3d9b4e59a0b0
jiri.one <nemec@jiri.one>
2020-08-20 15:23:33 +0200

    added url helper function

--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 from settings import posts_per_page, templatelookup
+import re
+from unidecode import unidecode
 
 def render_template(req, resp, resource, template):
 	"""@falcon.after decorator for Mako templates - works on GET and POST methodes"""
@@ -10,3 +12,12 @@ def slice_posts(page_number):
 	end = posts_per_page * page_number - 1
 	start = end - posts_per_page + 1
 	return start, end
+
+def create_url(header):
+	"""Function for create url adress from header of post."""
+	header = unidecode(header).lower() # firstly make all character lower and remove diacritics
+	pattern = re.compile(r"\W") # \W means everythink non-alphanumeric
+	splited_header = pattern.split(header) # split header with \W
+	splited_header = [i for i in splited_header if i] # list comprehension for remove empty strings from list
+	url = "-".join(splited_header) # and finaly join the list splited_header with "-"
+	return url
